a8a2dbfe57783f64ffb3fbefcbd340856a5aaecf
Aleksandr <Alfa>
2023-10-19 23:52:14 +0300

    added Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int SumOfNaturalElementsInTheInterval(int m, int n)
+{
+    if (m > n)
+        return m + SumOfNaturalElementsInTheInterval(m - 1, n);
+    if (m < n)
+        return n + SumOfNaturalElementsInTheInterval(m, n - 1);
+    else return m;
+}
+
+Console.WriteLine("Введите натуральное число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberM < 1 || numberN < 1)
+{
+    Console.WriteLine("Некорректный ввод!");
+    return;
+}
+
+Console.Write($"M = {numberM}; N = {numberN} -> {SumOfNaturalElementsInTheInterval(numberM, numberN)}");
